17f302768778bcab9dd5c3c86b397d5e980e4795
Adhithia Irvan <adhithia.irvan@gmail.com>
2023-08-07 19:43:50 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/api", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message":     "ok",
+			"app_version": "1",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
