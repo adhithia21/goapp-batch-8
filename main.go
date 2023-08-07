ce2b6516bf6efdee83b4b9450ae02a8c5ca775d6
Adhithia Irvan <adhithia.irvan@gmail.com>
2023-08-07 21:10:41 +0700

    update app verstion to 3

--- main.go
+++ main.go
@@ -10,8 +10,9 @@ func main() {
 	r := gin.Default()
 	r.GET("/api", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message":     "ok",
-			"app_version": "2",
+			"message":         "ok",
+			"app_version":     "3",
+			"app_description": "belajar gitops kubernetes with flux cd",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
