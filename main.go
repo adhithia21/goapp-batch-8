c5a65188659965e10fdcaef1c750357673adf82e
Adhithia Irvan <adhithia.irvan@gmail.com>
2023-08-21 19:51:13 +0700

    first commit

--- main.go
+++ main.go
@@ -11,8 +11,8 @@ func main() {
 	r.GET("/api", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message":         "ok",
-			"app_version":     "3",
-			"app_description": "belajar gitops kubernetes with flux cd",
+			"app_version":     "1",
+			"app_description": "deploy app jenkins",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
