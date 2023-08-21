ec0976fbc1ccf46a46cc17ee0c142dddeb732fbb
Adhithia Irvan <adhithia.irvan@gmail.com>
2023-08-21 21:08:03 +0700

    update app version

--- main.go
+++ main.go
@@ -11,8 +11,8 @@ func main() {
 	r.GET("/api", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message":         "ok",
-			"app_version":     "1",
-			"app_description": "deploy app jenkins",
+			"app_version":     "2.0",
+			"app_description": "deploy app jenkins version 2.0 build 2",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
